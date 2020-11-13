a53245d6dc0038600b982ba441ee9a0bf0cc24f0
Mujahidul Islam <32108024+Mujahid2923@users.noreply.github.com>
2020-11-13 19:17:02 +0600

    Update STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -5,6 +5,7 @@ s.substr( 0, 2 ) -> 0 theke 2 length
 set< char > st = { 'a','e','i','o','u' } ;
 vector< int > v ( n, 0 ) ; -> n size with value 0
 vector< vector<int> > dp( row, vector<int>(col, 1) ); => 2d vector with size row, col and initialize with 1 , like dp[ row ][ col ] ;
+s.erase( remove( s.begin(), s.end(), ' ' ), s.end() ) ; // remove white space from string
  
  -------------------------************----------------------------
 
